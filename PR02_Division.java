1bc247fa643d23c816f9b9e22cdcbd372cefc96e
Vichotte <Marcosjv1644@gmail.com>
2024-03-09 20:52:23 +0100

    Agregado JavaDoc a División

--- PR02_Division.java
+++ PR02_Division.java
@@ -2,8 +2,21 @@ package PRACTICA02;
 
 public class PR02_Division {
 
+    /**
+     * @author Marcos Jimenez Vicho
+     * @version 1.0
+     */
+
     private double operando1;
     private double operando2;
+
+    /**
+     *
+     * @param operando1 Es el operando numero 1 que el usuario mete en {@link PR02_Calculadora}
+     * @param operando2 Es el operando numero 2 que el usuario mete en {@link PR02_Calculadora}
+     * @return Devuelve el resultado de la division
+     * @serialData Hace una simple division con los parametros pasados por consola al usuario
+     */
 
     public static double division(double operando1, double operando2){
         double resul = 00.000;
