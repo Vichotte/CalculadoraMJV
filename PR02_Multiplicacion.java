99fc43836fc9ecbbb308bc36972462465237f6b5
Vichotte <Marcosjv1644@gmail.com>
2024-03-09 20:50:44 +0100

    Agregado JavaDoc a Multiplicación

--- PR02_Multiplicacion.java
+++ PR02_Multiplicacion.java
@@ -2,8 +2,21 @@ package PRACTICA02;
 
 public class PR02_Multiplicacion {
 
+    /**
+     * @author Marcos Jimenez Vicho
+     * @version 1.0
+     */
+
     private double operando1;
     private double operando2;
+
+    /**
+     *
+     * @param operando1 Es el operando numero 1 que el usuario mete en {@link PR02_Calculadora}
+     * @param operando2 Es el operando numero 2 que el usuario mete en {@link PR02_Calculadora}
+     * @return Devuelve el resultado de la multiplicacion
+     * @serialData Hace una simple multiplicacion con los parametros pasados por consola al usuario
+     */
 
     public static double multiplicacion(double operando1, double operando2){
 
